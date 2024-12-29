1cddfeea73716d32c9c2577dce338d9a73771418
Akhila9122 <akhilasaikam990@gmail.com>
2024-12-29 15:16:02 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#declaring of varible
+PERSON1=Akhila
+PERSON2=Saikam
+
+#reffering variable
 echo "Ramesh:: Hello Suresh, How are you?"
 echo "Suresh:: Hi Ramesh, I am fine. How are you?"
 echo "Ramesh: I am fine too. how is your work?"
